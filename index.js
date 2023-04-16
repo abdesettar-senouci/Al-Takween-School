73b1b2cc103c2b3d72ef8283140040c83eb3ee45
abdesettar <abdessettarsenouci@gmail.com>
2023-04-16 17:17:54 +0100

    test

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(flash());//not in use
 
 app.use(//cors()
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
